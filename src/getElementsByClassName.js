902bf4ba19dbe860015c513678ff5bd8a795e73d
Joshua Ding <joshding@github.com>
2020-10-05 12:42:09 -0700

    getElementsByClassName in progress

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,41 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+// Interpret the prompt: implementing getElementsByClassName recursively. with document.body, element.childNodes, element.classList
+// I: className (string) O: array of elements C: E:
+// Start from document.body, get class list, if a class matches className, push element onto array, then call getElementsBy ClassName for each child, if child === undefined, return array
+
+var getElementsByClassName = function(className, nodes
 ) {
-  // your code here
+  // set node as default to document.body. if other node is passed in as argument, use that.
+  nodes = nodes || [document.body];
+
+  //initialize array
+  var matches = [];
+  console.log('nodes: ',nodes);
+  // get the classList of the nodes.
+  for (var i = 0; i < nodes.length; i++) {
+    var classes = nodes[i].classList;
+    console.log('classes: ', classes);
+
+    //iterate through the classLists. If contains className, push node onto an array.
+    if (classes) {
+      for (var j = 0; j < classes.length; j++) {
+        if (classes[j] === className) {
+          matches.push(nodes[i].tagName.toLowerCase());
+        }
+      }
+      console.log('matches: ', matches); // matches: [body.targetClassName]
+      //all i want is ["body"];
+    }
+    //if there are children of this element, call getElementsByClassName(className, children). store that into a variable.
+    if (nodes[i].childNodes) {
+      matches = matches.concat(getElementsByClassName(className, nodes[i].childNodes));
+    }
+  }
+  //return the recursive variable.concat(array);
+  console.log('result: ', matches);
+
+  return matches;
+
 };
